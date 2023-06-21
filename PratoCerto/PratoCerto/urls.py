1a54019b4c09606d8d02be783c5ce70805cd6dd7
Ediel R. S. Caetano <edielromily123@gmail.com>
2023-06-20 21:14:46 -0300

    att

--- PratoCerto/PratoCerto/urls.py
+++ PratoCerto/PratoCerto/urls.py
@@ -15,10 +15,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('cozinha/', admin.site.urls),
-    path('cozinha/', admin.site.urls),
+    path('cozinha/', include('cozinha.urls')),
+    path('caixa/', include('caixa.urls')),
+    path('cliente/', include('cliente.urls')),
+    path('garcom/', include('garcom.urls')),
 ]
