6685bd8ea243ac228640730d8f9756ce6ca19c83
nevesfg <jaojose711@gmail.com>
2023-07-08 13:06:03 -0300

    Atualizaçõ del pagino del logon

---
+++ static/js/main.js
@@ -0,0 +1,23 @@
+const togglePassword = document.querySelector(".toggle-password");
+const passwordField = document.querySelector(togglePassword.getAttribute("toggle"));
+
+togglePassword.addEventListener("click", function () {
+  const type = passwordField.getAttribute("type") === "password" ? "text" : "password";
+  passwordField.setAttribute("type", type);
+  togglePassword.classList.toggle("fa-eye-slash");
+});
+
+document.getElementById("loginButton").addEventListener("click", function (event) {
+  event.preventDefault();
+  var usernameInput = document.getElementById("username-field");
+  var passwordInput = document.getElementById("password-field");
+
+  if (usernameInput.value && passwordInput.value) {
+    var loadingOverlay = document.getElementById("loadingOverlay");
+    loadingOverlay.style.display = "block";
+
+    setTimeout(function () {
+      document.getElementById("loginForm").submit();
+    }, 3500);
+  }
+});
