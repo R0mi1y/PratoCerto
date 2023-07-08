94661b1484a3ce321cbd79905c064778e1e3d6eb
r0mi1y <edielromily123@gmail.com>
2023-07-08 10:48:26 -0300

    Co-authored-by: Adauto Benevides <AdautoBenevides@users.noreply.github.com> Co-authored-by: Larissa Souza <larissasouza201565@gmail.com>

--- admin_gerente/urls.py
+++ admin_gerente/urls.py
@@ -5,5 +5,8 @@ urlpatterns = [
     path("home", views.home, name="home_admin"),
     path("criar_superuser", views.criar_editar_admin, name="create_superuser"),
     path("editar_superuser/<int:id>", views.criar_editar_admin, name="create_superuser"),
- 
+    
+    path("recomendacoes", views.ver_recomendacoes, name="gerenciar_recomendacoes"),
+    path("recomendacoes/deletar/<int:id>", views.deletar_recomendacao, name="deletar_recomendacoes"),
+    path("recomendacoes/adicionar/<int:id>", views.add_recomendacao, name="adicionar_recomendacoes"),
 ]
